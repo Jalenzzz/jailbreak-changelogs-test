d3fb2923d92c753ecf3274a391ee710d089f547f
Jalenzz <126164913+Jalenzzz@users.noreply.github.com>
2024-12-19 03:32:46 +0300

    back 2 top

--- scripts/values.js
+++ scripts/values.js
@@ -32,6 +32,24 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   );
 
+  const backToTopButton = document.getElementById("back-to-top");
+  // Show button when scrolling down 300px
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      backToTopButton.style.display = "flex";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+
+  // Scroll to top when button is clicked
+  backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
   // Function to load more items
   async function loadMoreItems() {
     if (isLoading) return;
