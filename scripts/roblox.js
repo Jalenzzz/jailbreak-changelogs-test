16d752090ffda69f723e53b5c1275e124f3cab7d
v3kmmw <v3kmww@gmail.com>
2024-12-21 21:27:15 -0600

    roblox integration?

---
+++ scripts/roblox.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here...
+    const redirect = 'https://authorize.roblox.com/?client_id=8033575152059272055&redirect_uri=https://jailbreakchangelogs.xyz/roblox&scope=openid%20profile&response_type=code'
+    const test_redirect = 'https://authorize.roblox.com/?client_id=8033575152059272055&redirect_uri=https://testing.jailbreakchangelogs.xyz/roblox&scope=openid%20profile&response_type=code'
+    console.log('Hello from Roblox');
+    document.getElementById('button').addEventListener('click', () => {
+        console.log('Redirecting to Roblox OAuth...');
+        window.location.href = test_redirect;
+    });
+    if (window.location.search.includes("code=")) {
+        const code = new URLSearchParams(window.location.search).get("code");
+        console.log('Code:', code);
+        fetch(`https://api.jailbreakchangelogs.xyz/roblox/auth?code=${code}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+    }
+});
