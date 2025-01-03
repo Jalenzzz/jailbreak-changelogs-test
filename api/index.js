d5dae7aa30e9de36d1f1ee5c4a8b5c51129c676c
v3kmmw <v3kmww@gmail.com>
2025-01-03 03:29:47 -0600

    new user endpoint
    this page is now severely broken!

--- api/index.js
+++ api/index.js
@@ -451,7 +451,7 @@ app.get("/users/:user/followers", async (req, res) => {
   if (!showfollowers) {
     // User has hidden their followers
     const userData = await fetch(
-      `https://api.jailbreakchangelogs.xyz/users/get?id=${user}`,
+      `https://api3.jailbreakchangelogs.xyz/users/get?id=${user}`,
       {
         headers: {
           "Content-Type": "application/json",
